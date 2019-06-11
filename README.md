d995eb64a561844bcafbe0bacdb9297a6cd624c9
Joao Pires <joaoc.pires@icloud.com>
2019-06-11 06:48:08 +0100

    Added documentation

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # ImageStore
 
-A description of this package.
+ImageStore is a light weight library that allows to fetch images from an URL and cache them for continuous use.
+It is designed to use with SwiftUI and Swift 5.1 and up.
+
+## Instalation
+### Swift Package Manager
+1. Copy the git URL 
+2. On Xcode, go to File -> Swift Packages -> Add Package Dependency
+3. Paste the URL
+4. Configure the desired versions
+
+## Usage
+On the view you wish to load the image, add a @EnvironmentObject of type ImageStore
+On that same view add a method that will load the image, eg fetch()
+Have an Image object, referencing the ImageStore.image
+When the view is loaded, call the fetch() method and send the url you wish to load.
+
+    struct ViewThumbnail : View {
+        var thumbnailUrl: String
+        @EnvironmentObject var imageStore: ImageStore
+
+        var body: some View {
+            let screenSize = UIScreen.main.bounds
+            return Image(uiImage: imageStore.image)
+                .resizable()
+                .aspectRatio(CGSize(width: 16, height: 9), contentMode: .fit)
+                .frame(width: 100, height: 100)
+                .clipped(antialiased: true)
+                .onAppear(perform: fetch)
+        }
+    
+        private func fetch() {
+            imageStore.getImage(from: thumbnailUrl)
+        }
+    
+    }
+
+
+### Note
+The package creates a folder where it store the images. after the initial load the images are stored in cache.
+Persistence is maintained for 30 days before a new request is needed.
+Requests are made automaticly.
--- Sources/ImageStore/ImageStore.swift
+++ Sources/ImageStore/ImageStore.swift
@@ -1,3 +1,129 @@
-struct ImageStore {
-    var text = "Hello, World!"
+//
+//  ImageStore.swift
+//  aCultural
+//
+//  Created by Joao Pires on 09/06/2019.
+//  Copyright © 2019 Joao Pires. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+import Combine
+
+@available(swift, introduced: 5.1)
+class ImageStore: BindableObject {
+    // MARK: - Properties
+    static var images: [URL: UIImage] = [:]
+    var didChange = PassthroughSubject<ImageStore, Never>()
+    var documentsDirectory: URL
+    var image: UIImage = UIImage() {
+        didSet {
+            DispatchQueue.main.async {
+                self.didChange.send(self)
+            }
+        }
+    }
+
+    // MARK: - Initializer
+    init() {
+        let manager = FileManager.default
+        let paths = manager.urls(for: .documentDirectory, in: .userDomainMask)
+        documentsDirectory = paths[0].appendingPathComponent("Images", isDirectory: true)
+        do {
+            try manager.createDirectory(at: documentsDirectory, withIntermediateDirectories: true, attributes: nil)
+        }
+        catch {
+            print("Debug Error: \(error.localizedDescription)")
+        }
+    }
+
+    // MARK: - Public Method
+    /// Requests an image from the given link. Subsequent requests will load the cached image for a maximum of 30 days.
+    /// The return value will be as a bindable ImageStore object, with an image property.
+    /// - Parameter urlString: a url that directs to an image.
+    func getImage(from urlString: String) {
+
+        DispatchQueue.global().async {
+
+            guard let url = URL(string: urlString) else { return }
+            if !self.loadFromCache(url) {
+
+                self.loadImage(fromUrl: url)
+                if let data = try? Data(contentsOf: url) {
+
+                    self.save(data, forUrl: url)
+                    guard let loadedImage = UIImage(data: data) else { return }
+                    self.image = loadedImage
+                }
+            }
+        }
+    }
+
+    // MARK: - Private Methods
+    private func save(_ data: Data, forUrl url: URL) {
+
+        let imageName = url.lastPathComponent
+        let imageDestinationUrl = documentsDirectory.appendingPathComponent(imageName, isDirectory: false)
+        if saveToDisk(url) {
+            do {
+                try data.write(to: imageDestinationUrl, options: [])
+            }
+            catch {
+                print("Debug Error: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    private func loadImage(fromUrl url: URL) {
+
+        let imageName = url.lastPathComponent
+        let imageDestinationUrl = documentsDirectory.appendingPathComponent(imageName, isDirectory: false)
+        if let data = try? Data.init(contentsOf: imageDestinationUrl) {
+
+            guard let loadedImage = UIImage(data: data) else { return }
+            self.image = loadedImage
+            DispatchQueue.main.async {
+
+                ImageStore.images.updateValue(loadedImage, forKey: url)
+            }
+        }
+    }
+
+    private func loadFromCache(_ url: URL) -> Bool {
+
+        if let image = ImageStore.images[url] {
+            self.image = image
+            return true
+        }
+        return false
+    }
+
+    private func saveToDisk(_ url: URL) -> Bool {
+
+        let userDefaults = UserDefaults.standard
+        let imageName = url.lastPathComponent
+        if let savedDate = userDefaults.value(forKey: imageName) as? Date {
+
+            let currentDate = Date()
+            let currentTime = currentDate.timeIntervalSince1970
+            let savedTime = savedDate.timeIntervalSince1970
+            let difference = currentTime - savedTime
+            // Reference: 30 days
+            let reference = 60 * 60 * 24 * 30
+            if Int(difference) > reference {
+
+                userDefaults.set(Date(), forKey: imageName)
+                return true
+            }
+            else {
+
+                return false
+            }
+        }
+        else {
+
+            userDefaults.set(Date(), forKey: imageName)
+            return true
+        }
+    }
 }
